fb1cc74f88d3c81ee9a52783d353689432f4ef8f
Davi <davi@agst.dev>
2021-03-05 10:26:35 -0300

    Trabalho semana 1 adicionado

---
+++ Trabalhos Semanais/Semana 1/Exercício 2..5.hs
@@ -0,0 +1,11 @@
+-- Exercício 2
+duplicar x = x * 2
+
+-- Exercício 3
+quadruple x = duplicar (duplicar x)
+
+-- Exercício 4
+hipotenusa a b = sqrt(a**2 + b**2)
+
+-- Exercício 5
+distancia x1 y1 x2 y2 = sqrt((x1 - x2)**2 + (y1 - y2)**2)
